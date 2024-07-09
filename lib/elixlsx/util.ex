ba4aa3a7eca81dbf800337f52cfca350ee0a4134
vinicius-remote <vinicius@remote.com>
2024-07-09 10:58:25 -0300

    feat: date validations (#4)

--- lib/elixlsx/util.ex
+++ lib/elixlsx/util.ex
@@ -1,5 +1,8 @@
 defmodule Elixlsx.Util do
   alias Elixlsx.XML
+
+  @type date :: :calendar.date()
+
   @col_alphabet Enum.to_list(?A..?Z)
 
   @doc ~S"""
@@ -136,7 +139,7 @@ defmodule Elixlsx.Util do
 
     to_string(
       :io_lib.format(
-        '~4.10.0b-~2.10.0b-~2.10.0bT~2.10.0b:~2.10.0b:~2.10.0bZ',
+        ~c"~4.10.0b-~2.10.0b-~2.10.0bT~2.10.0b:~2.10.0b:~2.10.0bZ",
         [y, m, d, hours, minutes, seconds]
       )
     )
@@ -194,6 +197,9 @@ defmodule Elixlsx.Util do
   Timestampts that are already in excel format are passed through
   unmodified.
   """
+  @spec to_excel_datetime(date) :: {:excelts, number}
+  def to_excel_datetime({yy, mm, dd}), do: to_excel_datetime({{yy, mm, dd}, {0, 0, 0}})
+
   @spec to_excel_datetime(datetime_t) :: {:excelts, number}
   def to_excel_datetime({{yy, mm, dd}, {h, m, s}}) do
     in_seconds = :calendar.datetime_to_gregorian_seconds({{yy, mm, dd}, {h, m, s}})
